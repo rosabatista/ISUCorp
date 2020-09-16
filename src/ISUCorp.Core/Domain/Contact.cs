cfa74cec718d63c9d400453ea50045ee8cdde5ee
Rosa Maria Batista Castillo <rosa.batista@itexico.com>
2020-09-16 16:54:38 -0500

    ISUCorp.Core project was added to the solution with the definition of the main entities.

---
+++ src/ISUCorp.Core/Domain/Contact.cs
@@ -0,0 +1,34 @@
+﻿using ISUCorp.Core.Kernel;
+using System;
+using System.Collections.Generic;
+
+namespace ISUCorp.Core.Domain
+{
+    public class Contact : BaseEntity
+    {
+        /// <summary>
+        /// Name of the contact.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Contact type.
+        /// </summary>
+        public ContactType Type { get; set; }
+
+        /// <summary>
+        /// Phone of the contact.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Birth date of the contact.
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Reservations made for the contact.
+        /// </summary>
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ src/ISUCorp.Core/Kernel/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ISUCorp.Core.Kernel
+{
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// Entity identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Create time
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// Update time
+        /// </summary>
+        public DateTime? UpdateTime { get; set; }
+
+        /// <summary>
+        /// Delete time
+        /// </summary>
+        public DateTime? DeleteTime { get; set; }
+    }
+}
